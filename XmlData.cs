0075f650a3a18db321815d8e9ee507c39bf9894f
EveKS <vikkes@yandex.ru>
2016-10-27 00:04:52 +0700

    Добавил дата класс

--- XmlData.cs
+++ XmlData.cs
@@ -1,11 +1,7 @@
-﻿using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
+﻿using System.Collections.Generic;
 using System.Xml.Serialization;
 
-namespace Reflection
+namespace Xml_To_Excel
 {
     [XmlRoot(ElementName = "a")]
     public class A
